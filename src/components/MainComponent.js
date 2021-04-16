d07addcf3b53a750e027445ef7097562d69abfd1
Amit Sahu <amitsahu.ltp@gmail.com>
2021-04-17 02:38:56 +0530

    Minor Fix

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -31,10 +31,6 @@ const mapDispatchToProps = (dispatch) => {
 
 class Main extends Component{
 
-    constructor(props){
-        super(props);
-    }
-
     render(){
         return (
             <Paper square style={{minHeight:'100vh'}}>
